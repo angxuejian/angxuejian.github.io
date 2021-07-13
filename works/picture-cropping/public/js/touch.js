6c20a16d182dbb94a7d875985569ec47a8822aae
angxuejian <xuejian.ang@gmail.com>
2021-07-13 17:44:57 +0800

    test: edit picture-cropping works

--- works/picture-cropping/public/js/touch.js
+++ works/picture-cropping/public/js/touch.js
@@ -51,7 +51,6 @@ function dragMove(touches) {
   T_IMG.style.transform = `translate3d(${left}px, ${top}px, 0) scale(${T_IMG_.scale})`
 }
 
-
 /**
  * 单指拖动事件 - 结束
  * 超出边界后 回退至边界
@@ -86,4 +85,97 @@ function dragEnd() {
     CTX_DRAW.y = T_IMG_.top - BOX_.top === T_IMG_.diffH ? T_IMG_.top - BOX_.top : T_IMG_.top - Math.abs(T_IMG_.diffH)
     CTX_DRAW.x = T_IMG_.left - BOX_.left === T_IMG_.diffW ? T_IMG_.left - BOX_.left : T_IMG_.left - Math.abs(T_IMG_.diffW)
 
+}
+
+
+
+// 缩放事件所需参数
+const SCALE = {
+  top      : 0,
+  left     : 0,
+  distance1: 0,
+  distance2: 0,
+  xx       : 0,
+  yy       : 0,
+  scale    : 1
+}
+
+/**
+ * 双指缩放事件 - 开始
+ * @param {Array[Object]} touches 手指触摸页面时的参数
+ */
+function scaleStart(touches) {
+  isDrag = false
+
+  SCALE.distance1 = getDistance(touches[0], touches[1])
+  SCALE.scale = T_IMG_.scale
+}
+
+/**
+ * 双指缩放事件 - 缩放
+ * @param {Array[Object]} touches 手指触摸页面时的参数
+ */
+function scaleMove(touches) {
+  SCALE.distance2 = getDistance(touches[0], touches[1])
+
+  const zoom = SCALE.distance2 / SCALE.distance1
+  
+  let scale = SCALE.scale * zoom
+
+  if (scale > 3) {
+    scale = 3
+  }
+
+  T_IMG_.scale = scale
+  
+  T_IMG.style.transform = `translate3d(${T_IMG_.left}px, ${T_IMG_.top}px, 0) scale(${T_IMG_.scale})`
+
+  let h  = T_IMG_.h * T_IMG_.scale
+  let w  = T_IMG_.w * T_IMG_.scale
+  let a = (window.innerHeight - h) / 2
+  let b = (window.innerWidth  - w) / 2
+  T_IMG_.diffH = a  - BOX_.top
+  T_IMG_.diffW = b - BOX_.left
+
+  CTX_DRAW.x = b - BOX_.left
+  CTX_DRAW.y = a - BOX_.top
+  CTX_DRAW.h = h
+  CTX_DRAW.w = w
+}
+
+
+function scaleEnd() {
+
+  if (T_IMG_.scale < 1) {
+    T_IMG_.scale = 1
+    
+    T_IMG.style.transform = `translate3d(${T_IMG_.left}px, ${T_IMG_.top}px, 0) scale(${T_IMG_.scale})`
+    
+    let h  = T_IMG_.h * T_IMG_.scale
+    let w  = T_IMG_.w * T_IMG_.scale
+    let a = (window.innerHeight - h) / 2
+    let b = (window.innerWidth  - w) / 2
+    T_IMG_.diffH = a  - BOX_.top
+    T_IMG_.diffW = b - BOX_.left
+  
+    CTX_DRAW.x = b - BOX_.left
+    CTX_DRAW.y = a - BOX_.top
+    CTX_DRAW.h = h
+    CTX_DRAW.w = w
+  }
+  dragEnd()
+}
+
+
+/**
+ * 
+ * @param {Object} start touches[0]
+ * @param {Object} end   touches[1]
+ * @returns 两指的距离
+ */
+function getDistance(start, end) {
+  let s = Math.abs(end.pageX - start.pageX) * 2
+  let e = Math.abs(end.pageY - start.pageY) * 2
+
+  return Math.sqrt(s + e)
 }
